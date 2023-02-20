9fa00a140953793b5c8d66bb2a5193c370259750
Ian Levesque <ian@ianlevesque.org>
2023-02-19 22:11:26 -0500

    Apply ktfmt formatting (#1)
    Summary: Setup ktfmt for consistent formatting. Formatter can be run
    with 'gradle ktfmtFormat'
    
    Reviewed By: nghuiqin
    
    Differential Revision: D43415681
    
    Co-authored-by: Ian Levesque <ianlevesque@meta.com>

--- core/src/test/kotlin/com/tschuchort/compiletesting/KotlinTestProcessor.kt
+++ core/src/test/kotlin/com/tschuchort/compiletesting/KotlinTestProcessor.kt
@@ -13,82 +13,88 @@ import javax.tools.Diagnostic
 annotation class ProcessElem
 
 data class ProcessedElemMessage(val elementSimpleName: String) {
-	fun print() = MSG_PREFIX + elementSimpleName + MSG_SUFFIX
+  fun print() = MSG_PREFIX + elementSimpleName + MSG_SUFFIX
 
-	init {
-	    require(elementSimpleName.isNotEmpty())
-	}
+  init {
+    require(elementSimpleName.isNotEmpty())
+  }
 
-	companion object {
-		private const val MSG_PREFIX = "processed element{"
-		private const val MSG_SUFFIX = "}"
+  companion object {
+    private const val MSG_PREFIX = "processed element{"
+    private const val MSG_SUFFIX = "}"
 
-	    fun parseAllIn(s: String): List<ProcessedElemMessage> {
-			val pattern = Regex(Regex.escape(MSG_PREFIX) + "(.+)?" + Regex.escape(MSG_SUFFIX))
-			return pattern.findAll(s)
-				.map { match ->
-					ProcessedElemMessage(match.destructured.component1())
-				}.toList()
-		}
-	}
+    fun parseAllIn(s: String): List<ProcessedElemMessage> {
+      val pattern = Regex(Regex.escape(MSG_PREFIX) + "(.+)?" + Regex.escape(MSG_SUFFIX))
+      return pattern
+          .findAll(s)
+          .map { match -> ProcessedElemMessage(match.destructured.component1()) }
+          .toList()
+    }
+  }
 }
 
 class KotlinTestProcessor : AbstractProcessor() {
-	companion object {
-		private const val KAPT_KOTLIN_GENERATED_OPTION_NAME = "kapt.kotlin.generated"
-		private const val GENERATE_KOTLIN_CODE_OPTION = "generate.kotlin.value"
-		private const val GENERATE_ERRORS_OPTION = "generate.error"
-		private const val FILE_SUFFIX_OPTION = "suffix"
-		const val ON_INIT_MSG = "kotlin processor init"
-		const val GENERATED_PACKAGE = "com.tschuchort.compiletesting"
-		const val GENERATED_JAVA_CLASS_NAME = "KotlinGeneratedJavaClass"
-		const val GENERATED_KOTLIN_CLASS_NAME = "KotlinGeneratedKotlinClass"
-	}
+  companion object {
+    private const val KAPT_KOTLIN_GENERATED_OPTION_NAME = "kapt.kotlin.generated"
+    private const val GENERATE_KOTLIN_CODE_OPTION = "generate.kotlin.value"
+    private const val GENERATE_ERRORS_OPTION = "generate.error"
+    private const val FILE_SUFFIX_OPTION = "suffix"
+    const val ON_INIT_MSG = "kotlin processor init"
+    const val GENERATED_PACKAGE = "com.tschuchort.compiletesting"
+    const val GENERATED_JAVA_CLASS_NAME = "KotlinGeneratedJavaClass"
+    const val GENERATED_KOTLIN_CLASS_NAME = "KotlinGeneratedKotlinClass"
+  }
 
-	private val kaptKotlinGeneratedDir by lazy { processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME] }
+  private val kaptKotlinGeneratedDir by lazy {
+    processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME]
+  }
 
-	override fun getSupportedSourceVersion(): SourceVersion = SourceVersion.latest()
-	override fun getSupportedOptions() = setOf(
-		KAPT_KOTLIN_GENERATED_OPTION_NAME,
-		GENERATE_KOTLIN_CODE_OPTION,
-		GENERATE_ERRORS_OPTION
-	)
-	override fun getSupportedAnnotationTypes(): Set<String> = setOf(ProcessElem::class.java.canonicalName)
+  override fun getSupportedSourceVersion(): SourceVersion = SourceVersion.latest()
+  override fun getSupportedOptions() =
+      setOf(KAPT_KOTLIN_GENERATED_OPTION_NAME, GENERATE_KOTLIN_CODE_OPTION, GENERATE_ERRORS_OPTION)
+  override fun getSupportedAnnotationTypes(): Set<String> =
+      setOf(ProcessElem::class.java.canonicalName)
 
-    override fun init(processingEnv: ProcessingEnvironment) {
-        processingEnv.messager.printMessage(Diagnostic.Kind.WARNING, ON_INIT_MSG)
-        super.init(processingEnv)
+  override fun init(processingEnv: ProcessingEnvironment) {
+    processingEnv.messager.printMessage(Diagnostic.Kind.WARNING, ON_INIT_MSG)
+    super.init(processingEnv)
+  }
+
+  override fun process(annotations: Set<TypeElement>, roundEnv: RoundEnvironment): Boolean {
+    processingEnv.messager.printMessage(Diagnostic.Kind.WARNING, "kotlin processor was called")
+
+    for (annotatedElem in roundEnv.getElementsAnnotatedWith(ProcessElem::class.java)) {
+      processingEnv.messager.printMessage(
+          Diagnostic.Kind.WARNING,
+          ProcessedElemMessage(annotatedElem.simpleName.toString()).print())
     }
 
-	override fun process(annotations: Set<TypeElement>, roundEnv: RoundEnvironment): Boolean {
-		processingEnv.messager.printMessage(Diagnostic.Kind.WARNING, "kotlin processor was called")
+    if (annotations.isEmpty()) {
+      FileSpec.builder(GENERATED_PACKAGE, GENERATED_KOTLIN_CLASS_NAME + ".kt")
+          .addType(TypeSpec.classBuilder(GENERATED_KOTLIN_CLASS_NAME).build())
+          .build()
+          .let { writeKotlinFile(it) }
 
-		for (annotatedElem in roundEnv.getElementsAnnotatedWith(ProcessElem::class.java)) {
-			processingEnv.messager.printMessage(Diagnostic.Kind.WARNING,
-				ProcessedElemMessage(annotatedElem.simpleName.toString()).print())
-		}
+      JavaFile.builder(
+              GENERATED_PACKAGE, JavaTypeSpec.classBuilder(GENERATED_JAVA_CLASS_NAME).build())
+          .build()
+          .writeTo(processingEnv.filer)
+    }
 
-		if(annotations.isEmpty()) {
-			FileSpec.builder(GENERATED_PACKAGE, GENERATED_KOTLIN_CLASS_NAME + ".kt")
-					.addType(
-						TypeSpec.classBuilder(GENERATED_KOTLIN_CLASS_NAME).build()
-					).build()
-					.let { writeKotlinFile(it) }
+    return false
+  }
 
-			JavaFile.builder(GENERATED_PACKAGE, JavaTypeSpec.classBuilder(GENERATED_JAVA_CLASS_NAME).build())
-					.build().writeTo(processingEnv.filer)
-		}
+  private fun writeKotlinFile(
+      fileSpec: FileSpec,
+      fileName: String = fileSpec.name,
+      packageName: String = fileSpec.packageName
+  ) {
+    val relativePath = packageName.replace('.', File.separatorChar)
 
-		return false
-	}
+    val outputFolder = File(kaptKotlinGeneratedDir!!, relativePath)
+    outputFolder.mkdirs()
 
-	private fun writeKotlinFile(fileSpec: FileSpec, fileName: String = fileSpec.name, packageName: String = fileSpec.packageName) {
-		val relativePath = packageName.replace('.', File.separatorChar)
-
-		val outputFolder = File(kaptKotlinGeneratedDir!!, relativePath)
-		outputFolder.mkdirs()
-
-		// finally write to output file
-		File(outputFolder, fileName).writeText(fileSpec.toString())
-	}
+    // finally write to output file
+    File(outputFolder, fileName).writeText(fileSpec.toString())
+  }
 }
